130635922acfa4832bcea7ee88e6be07df9df786
mozix5 <mosinmd827@gmail.com>
2024-04-01 19:19:22 +0530

    added padding and changed font size

--- src/components/HomeBanner.jsx
+++ src/components/HomeBanner.jsx
@@ -3,15 +3,21 @@ import React from "react";
 const HomeBanner = () => {
   return (
     <div className=" bg-dark-blue text-white p-large">
-      <div className=" text-[#d1f1ff]">PayPal Developer</div>
-      <div>Build a payment solution that's right for you.</div>
-      <div>
+      <div className=" text-[#d1f1ff] text-[28px] opacity-[0.7]">
+        PayPal Developer
+      </div>
+      <div className=" text-[44px] py-8">
+        Build a payment solution that's right for you.
+      </div>
+      <div className=" text-lg pb-8">
         Find the resources you need to test and go live with online, mobile, or
         in-person payment solutions.
       </div>
-      <button className=" bg-[#ffc43a] rounded-2xl px-4 py-2 text-dark-blue">
-        Get API Credentials
-      </button>
+      <div>
+        <button className=" bg-[#ffc43a] rounded-3xl px-6 py-2 text-dark-blue text-lg">
+          Get API Credentials
+        </button>
+      </div>
     </div>
   );
 };
