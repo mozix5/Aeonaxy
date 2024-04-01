f9f6e13c9d7eee7d7b0e0ba2fb235989c06806e2
mozix5 <mosinmd827@gmail.com>
2024-04-01 21:52:26 +0530

    Refactor Payments component: Extract repetitive structure into PaymentCategory component and added icons

--- src/pages/Payments.jsx
+++ src/pages/Payments.jsx
@@ -1,62 +1,67 @@
 import React from "react";
 import LinkText from "../components/LinkText";
 import PaymentsImage from "../svg/PaymentsHeroSvg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faDisplay,
+  faHandshakeSimple,
+  faShop,
+  faWallet,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Payments = () => {
   return (
     <div className="p-large flex">
       <div className="w-full">
-        <div className=" text-[44px]">Accept Payments</div>
-        <div className=" flex mt-8">
-          <div className="flex-1">
-            <div className=" text-[28px]">Online</div>
-            <div className=" text-lg py-4 w-[90%]">
-              Build a scalable checkout solution for web and mobile with our
-              REST APIs and JavaScript SDK
-            </div>
-            <div className=" flex gap-3 flex-col ">
-              <LinkText name="Checkout" />
-              <LinkText name="Subscriptions" />
-              <LinkText name="Invoicing" />
-              <LinkText name="Donate" />
-            </div>
-          </div>
-          <div className="flex-1">
-            <div className=" text-[28px]">In-Person</div>
-            <div className=" text-lg py-4 w-[90%]">
-              Set up a ready-made point-of-sale solution or integrate a payment
-              solution to your service
-            </div>
-            <div className=" flex gap-3 flex-col ">
-              <LinkText name="Point of Sale Solutions" />
-            </div>
-          </div>
+        <div className="text-4xl">Accept Payments</div>
+        <div className="flex mt-8">
+          <PaymentCategory
+            title="Online"
+            description="Build a scalable checkout solution for web and mobile with our REST APIs and JavaScript SDK"
+            links={["Checkout", "Subscriptions", "Invoicing", "Donate"]}
+            icon={faDisplay}
+          />
+          <PaymentCategory
+            title="In-Person"
+            description="Set up a ready-made point-of-sale solution or integrate a payment solution to your service"
+            links={["Point of Sale Solutions"]}
+            icon={faWallet}
+          />
         </div>
         <div className="flex mt-10">
-          <div className="flex-1">
-            <div className=" text-[28px]">Multiparty</div>
-            <div className=" text-lg py-4 w-[90%]">
-              Create an onboarding and payments solution for your marketplace or
-              payments platform
-            </div>
-            <div className=" flex gap-3 flex-col ">
-              <LinkText name="Build a Marketplace" />
-            </div>
-          </div>
-          <div className="flex-1">
-            <div className=" text-[28px]">3rd-Party</div>
-            <div className=" text-lg py-4 w-[90%]">
-              Integrate PayPal with a 3rd-party shopping cart or plugin
-            </div>
-            <div className=" flex gap-3 flex-col ">
-              <LinkText name="Partners" />
-              <LinkText name="Plugins" />
-            </div>
-          </div>
+          <PaymentCategory
+            title="Multiparty"
+            description="Create an onboarding and payments solution for your marketplace or payments platform"
+            links={["Build a Marketplace"]}
+            icon={faShop}
+          />
+          <PaymentCategory
+            title="3rd-Party"
+            description="Integrate PayPal with a 3rd-party shopping cart or plugin"
+            links={["Partners", "Plugins"]}
+            icon={faHandshakeSimple}
+          />
         </div>
       </div>
-      <div className="w-[550px] pt-6">
+      <div className="w-550 pt-6">
         <PaymentsImage />
+      </div>
+    </div>
+  );
+};
+
+const PaymentCategory = ({ title, description, links, icon }) => {
+  return (
+    <div className="flex-1">
+      <div className="flex gap-4 items-center">
+        <FontAwesomeIcon icon={icon} className=" text-link-text text-xl" />
+        <div className="text-2xl">{title}</div>
+      </div>
+      <div className="text-lg py-4 w-[90%]">{description}</div>
+      <div className="flex gap-3 flex-col">
+        {links.map((link) => (
+          <LinkText key={link} name={link} />
+        ))}
       </div>
     </div>
   );
