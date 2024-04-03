408d6f56e82f73edd72d7d57d1d98fc90dacd1e1
mozix5 <mosinmd827@gmail.com>
2024-04-03 21:52:37 +0530

    added animation

--- src/components/NavbarMobile.jsx
+++ src/components/NavbarMobile.jsx
@@ -3,13 +3,14 @@ import NavItems from "./NavItems";
 import PrimaryButton from "./PrimaryButton";
 
 const NavbarMobile = ({ open, setter }) => {
-  const appendClass = open ? "ml-0" : "ml-[-380px]";
+  const appendClass = open
+    ? "ml-0 transition-all duration-300"
+    : "ml-[-380px] transition-all duration-300";
+
   const ModalOverlay = () => (
     <div
-      className={`flex lg:hidden overflow-y-hidden fixed top-0 right-0 bottom-0 left-0 bg-black/50 z-10`}
-      onClick={() => {
-        setter((oldVal) => !oldVal);
-      }}
+      className={`fixed top-0 right-0 bottom-0 left-0 bg-black/50 z-10 transition-opacity duration-300 ${open ? 'opacity-100' : 'opacity-0'}`}
+      onClick={() => setter((oldVal) => !oldVal)}
     />
   );
 
@@ -22,7 +23,6 @@ const NavbarMobile = ({ open, setter }) => {
           <NavItems />
         </div>
         <PrimaryButton name="Login" />
-        <div className=""></div>
       </div>
       {open && <ModalOverlay />}
     </>
